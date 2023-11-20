07eb69ea659c1d4fd4ffbda10a9c1cccdb811ebf
vbv_nyk <vbv.nyk.2003@gmail.com>
2023-11-20 18:59:20 +0530

    Adding methods to File Structure

--- src/directories/files/mod.rs
+++ src/directories/files/mod.rs
@@ -1,11 +1,14 @@
 pub mod file {
     use std::{
-        fs::{FileType, Permissions},
+        ffi::OsString,
+        fs::{DirEntry, FileType, Permissions},
+        path::PathBuf,
         time::SystemTime,
     };
 
-    struct File {
-        name: String,
+    pub struct File {
+        name: OsString,
+        path: PathBuf,
         file_type: FileType,
         is_dir: bool,
         is_file: bool,
@@ -18,6 +21,76 @@ pub mod file {
     }
 
     impl File {
-        fn new() -> File {}
+        pub fn new(f: &DirEntry) -> File {
+            let name = f.file_name();
+            let path = f.path();
+            let file_type = f.file_type().unwrap();
+            let is_dir = f.metadata().unwrap().is_dir();
+            let is_file = f.metadata().unwrap().is_file();
+            let is_symlink = f.metadata().unwrap().is_symlink();
+            let length = f.metadata().unwrap().len();
+            let permission = f.metadata().unwrap().permissions();
+            let modified = f.metadata().unwrap().modified().unwrap();
+            let accessed = f.metadata().unwrap().accessed().unwrap();
+            let created = f.metadata().unwrap().created().unwrap();
+
+            File {
+                name,
+                path,
+                file_type,
+                is_dir,
+                is_file,
+                is_symlink,
+                accessed,
+                created,
+                modified,
+                length,
+                permission,
+            }
+        }
+
+        pub fn get_name(&self) -> &OsString {
+            &self.name
+        }
+
+        pub fn get_path(&self) -> &PathBuf {
+            &self.path
+        }
+
+        pub fn get_file_type(&self) -> &FileType {
+            &self.file_type
+        }
+
+        pub fn is_dir(&self) -> bool {
+            self.is_dir
+        }
+
+        pub fn is_file(&self) -> bool {
+            self.is_file
+        }
+
+        pub fn is_symlink(&self) -> bool {
+            self.is_symlink
+        }
+
+        pub fn get_length(&self) -> u64 {
+            self.length
+        }
+
+        pub fn get_permission(&self) -> &Permissions {
+            &self.permission
+        }
+
+        pub fn get_modified_time(&self) -> &SystemTime {
+            &self.modified
+        }
+
+        pub fn get_accessed_time(&self) -> &SystemTime {
+            &self.accessed
+        }
+
+        pub fn get_created_time(&self) -> &SystemTime {
+            &self.created
+        }
     }
 }
--- src/directories/mod.rs
+++ src/directories/mod.rs
@@ -2,38 +2,47 @@ mod files;
 pub mod directory {
     use std::{
         ffi::OsString,
-        fs::{self, DirEntry, File},
+        fs::{self, DirEntry},
         io::Error,
+        path::PathBuf,
     };
 
+    use super::files::file::File;
+
     pub struct Directory {
-        pub contents: Vec<DirEntry>,
         pub files: Vec<File>,
     }
 
     impl Directory {
         pub fn new(path: Option<&str>) -> Directory {
-            let contents = init(path.unwrap_or_else(|| "."));
-            Directory {
-                contents,
-                files: vec![],
-            }
+            let current_directory = load_directories(path.unwrap_or("."));
+            match current_directory {
+                Ok(fnds) => {
+                    return Directory {
+                        files: load_files(&fnds),
+                    }
+                }
+                Err(e) => panic!("Couldn't open the files and the directories because {e}"),
+            };
         }
 
         pub fn get_file_names(&self) -> Vec<OsString> {
-            let file_names: Vec<OsString> =
-                self.contents.iter().map(|fnd| fnd.file_name()).collect();
+            let file_names: Vec<OsString> = self
+                .files
+                .iter()
+                .map(|file| file.get_name().clone())
+                .collect();
             for (i, file_name) in file_names.iter().enumerate() {
                 println!("{i}: {:#?}", file_name);
             }
             file_names
         }
 
-        pub fn get_file_paths(&self) -> Vec<OsString> {
-            let file_paths: Vec<OsString> = self
-                .contents
+        pub fn get_file_paths(&self) -> Vec<PathBuf> {
+            let file_paths: Vec<PathBuf> = self
+                .files
                 .iter()
-                .map(|fnd| fnd.path().into_os_string())
+                .map(|file| file.get_path().clone())
                 .collect();
 
             for (i, file_path) in file_paths.iter().enumerate() {
@@ -43,7 +52,7 @@ pub mod directory {
         }
     }
 
-    pub fn load_directories(path: &str) -> Result<Vec<DirEntry>, Error> {
+    fn load_directories(path: &str) -> Result<Vec<DirEntry>, Error> {
         let current_directory = fs::read_dir(path)?;
         let current_directory = current_directory.into_iter();
 
@@ -52,23 +61,12 @@ pub mod directory {
         Ok(fnds)
     }
 
-    pub fn load_contents(fnds: Result<Vec<DirEntry>, Error>) -> Vec<DirEntry> {
-        let fnds = match fnds {
-            Ok(fnds) => fnds,
-            Err(e) => panic!("Couldn't open the files and the directories because {e}"),
-        };
-
-        fnds
-    }
-
-    pub fn load_files(fnds: Result<Vec<DirEntry>, Error>) -> Vec<File> {
-        let fnds = match fnds {
-            Ok(fnds) => {}
-            Err(e) => panic!("Couldn't open the files and the directories because {e}"),
-        };
-    }
-
-    pub fn init(path: &str) -> Vec<DirEntry> {
-        load_contents(load_directories(path))
+    fn load_files(fnds: &Vec<DirEntry>) -> Vec<File> {
+        let mut files = vec![];
+        fnds.iter().for_each(|f| {
+            let file = File::new(f);
+            files.push(file);
+        });
+        files
     }
 }
