404208641f51362d9dd4274515ff6bcf6485ee64
vbv_nyk <vbv.nyk.2003@gmail.com>
2023-11-24 12:01:53 +0530

    Giving colors for directories

--- src/ui/common.rs
+++ src/ui/common.rs
@@ -1,5 +1,7 @@
 use std::os::fd;
 
+use ratatui::style::Color;
+use ratatui::style::Style;
 use ratatui::widgets::List;
 use ratatui::widgets::ListItem;
 use ratatui::widgets::Tabs;
@@ -44,6 +46,27 @@ pub fn fs_list_view(app: &App) -> List<'_> {
         // let dirs: Vec<ListItem> = dirs.iter().map(|f| ListItem::new(f.clone())).collect();
         let files: Vec<ListItem> = files.iter().map(|f| ListItem::new(f.clone())).collect();
         // fnds.extend(dirs);
+        fnds.extend(files);
+    }
+    let fnds = List::new(fnds);
+    fnds
+}
+
+pub fn fnds_list_view(app: &App) -> List<'_> {
+    let mut fnds: Vec<ListItem> = vec![];
+    if let States::NORMAL(tab) = app.get_state() {
+        let index = usize::try_from(*tab).unwrap();
+        let current_directory = app.get_directories().get(index).unwrap();
+        let dirs = current_directory.get_only_dirs();
+        let files = current_directory.get_only_files();
+        let dirs: Vec<ListItem> = dirs
+            .iter()
+            .map(|f| {
+                ListItem::new(f.clone()).style(Style::default().fg(Color::Blue).bg(Color::Black))
+            })
+            .collect();
+        let files: Vec<ListItem> = files.iter().map(|f| ListItem::new(f.clone())).collect();
+        fnds.extend(dirs);
         fnds.extend(files);
     }
     let fnds = List::new(fnds);
